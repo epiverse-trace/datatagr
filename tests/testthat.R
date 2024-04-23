bd4fae950116bf6865894612192a823f90cbfec4
Chris Hartgerink <chris@libscie.org>
2024-04-23 14:36:02 +0200

    Update template naming

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/articles/special-files.html
 
 library(testthat)
-library(packagetemplate)
+library(datatagr)
 
-test_check("packagetemplate", stop_on_warning = FALSE)
+test_check("datatagr", stop_on_warning = FALSE)

0ee251c62d25906db9578a42be4bafe9ef232222
Chris Hartgerink <chris@libscie.org>
2024-08-14 16:30:15 +0200

    Add `type()` and tests

---
+++ tests/testthat/test-type.R
@@ -0,0 +1,35 @@
+test_that("type() fails as expected", {
+  # incorrect argument type
+  expect_error(type(1))
+  # incorrect type choice
+  expect_error(type("123"))
+})
+
+test_that("type() returns all options as expected", {
+  x <- c("integer", "numeric", "Date", "POSIXct", "POSIXlt")
+  expect_equal(type("date"), x)
+
+  x <- c("character", "factor")
+  expect_equal(type("category"), x)
+
+  x <- c("numeric", "integer")
+  expect_equal(type("numeric"), x)
+
+  x <- c("logical", "integer", "character", "factor")
+  expect_equal(type("binary"), x)
+})
+
+test_that("type() works nicely when combined with validate_types()", {
+  x <- make_datatagr(cars,
+    mph = "speed",
+    distance = "dist"
+  )
+
+  validate_types(
+    x,
+    tags_types(
+      mph = type("numeric"),
+      distance = type("numeric")
+    )
+  )
+})

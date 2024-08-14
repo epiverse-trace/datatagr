c58a38bc8db517c74bd2b7044ce0f089dc4810d3
Chris Hartgerink <chris@libscie.org>
2024-08-14 16:30:15 +0200

    Add `type()` and tests

---
+++ R/type.R
@@ -0,0 +1,42 @@
+#' Type Selection Helper
+#'
+#' Function to swiftly provide access to generic categories of types within R.
+#' These can be used to provide comprehensive typesetting when creating a
+#' `datatagr` object.
+#'
+#' @param x Character indicating the desired type. Options include `date`,
+#' `category`, `numeric`, `binary` at this time.
+#'
+#' @return A vector of classes
+#' @export
+#'
+#' @examples
+#' x <- make_datatagr(cars,
+#'   mph = "speed",
+#'   distance = "dist"
+#' )
+#'
+#' validate_types(
+#'   x,
+#'   tags_types(
+#'     mph = type("numeric"),
+#'     distance = type("numeric")
+#'   )
+#' )
+#'
+type <- function(x) {
+  # ensure case insensitivity
+  x <- tolower(x)
+
+  checkmate::assert_string(x)
+  checkmate::assert_choice(x,
+    choices = c("date", "category", "numeric", "binary")
+  )
+
+  switch(x,
+    "date" = c("integer", "numeric", "Date", "POSIXct", "POSIXlt"),
+    "category" = c("character", "factor"),
+    "numeric" = c("numeric", "integer"),
+    "binary" = c("logical", "integer", "character", "factor")
+  )
+}

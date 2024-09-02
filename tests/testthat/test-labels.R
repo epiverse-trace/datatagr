09f4644e7628d750228101d1326a9cc21f644fd0
Chris Hartgerink <chris@libscie.org>
2024-09-02 16:24:00 +0200

    Rename `test-tags.R` to `test-labels.R`

--- tests/testthat/test-labels.R
+++ tests/testthat/test-labels.R
@@ -1,17 +1,9 @@
-test_that("labels() works on singular label datatagr objects", {
-    x <- make_datatagr(cars, speed = "Miles per hour")
-    y <- list(speed = 'Miles per hour')
-    expect_identical(labels(x), y)
-})
+test_that("tests for tags", {
+  # Check error messages
+  x <- make_datatagr(cars, speed = "Miles per hour")
 
-test_that("labels() works on datatagr objects with multiple labels", {
-  x <- make_datatagr(cars, speed = "Miles per hour", dist = "Distance")
-  y <- list(speed = 'Miles per hour', dist = 'Distance')
-  expect_identical(labels(x), y)
-})
-
-test_that("labels() shows NULL values as expected", {
-    x <- make_datatagr(cars, speed = "Miles per hour")
-    y <- list(speed = 'Miles per hour', dist = NULL)
-    expect_identical(labels(x, show_null = TRUE), y)
+  # Check functionality
+  expect_identical(labels(x), list(mph = "speed"))
+  expect_identical(labels(x, TRUE), attr(x, "tags"))
+  expect_identical(labels(make_datatagr(cars), TRUE), list())
 })
--- tests/testthat/test-tags.R
+++
@@ -1,9 +0,0 @@
-test_that("tests for tags", {
-  # Check error messages
-  x <- make_datatagr(cars, speed = "Miles per hour")
-
-  # Check functionality
-  expect_identical(labels(x), list(mph = "speed"))
-  expect_identical(labels(x, TRUE), attr(x, "tags"))
-  expect_identical(labels(make_datatagr(cars), TRUE), list())
-})

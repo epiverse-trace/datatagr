9b3cc345adfb38c1429a83836fd5792e631607a8
Chris Hartgerink <chris@libscie.org>
2024-09-03 09:50:54 +0200

    Refactor `has_tag()` to `has_label()`

---
+++ R/has_label.R
@@ -0,0 +1,33 @@
+#' A selector function to use in \pkg{tidyverse} functions
+#'
+#' @param labels A character vector of labels you want to operate on
+#'
+#' @returns A numeric vector containing the position of the columns with the
+#'   requested labels
+#'   
+#' @note Using this in a pipeline results in a 'datatagr' object, but does not
+#'   maintain the variable labels at this time.
+#'
+#' @export
+#'
+#' @examples
+#' ## create datatagr
+#' x <- make_datatagr(cars,
+#'   speed = "Miles per hour",
+#'   dist = "Distance in miles"
+#' )
+#' head(x)
+#'
+#' if (require(dplyr) && require(magrittr)) {
+#'   x %>%
+#'     select(has_label(c("Miles per hour", "Distance in miles"))) %>%
+#'     head()
+#' }
+has_label <- function(labels) {
+  dat <- tidyselect::peek_data(fn = "has_label")
+  dat_labels <- labels(dat)
+
+  cols_to_extract <- dat_labels[dat_labels %in% labels]
+
+  which(colnames(dat) %in% names(cols_to_extract))
+}

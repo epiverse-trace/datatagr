23c831b04287e2f97502af665f2c52cddc445d09
Chris Hartgerink <chris@libscie.org>
2024-09-03 15:33:36 +0200

    Update `datatagr-package` to use labelling

--- R/datatagr-package.R
+++ R/datatagr-package.R
@@ -1,9 +1,9 @@
-#' Base Tools for Tagging and Validating Data
+#' Base Tools for Labelling and Validating Data
 #'
-#' The *datatagr* package provides tools to help tag and validate data. The
-#' 'datatagr' class adds a custom tagging system to classical 'data.frame'
-#' objects to identify key data. Once tagged, these variables can be seamlessly
-#' used in downstream analyses, making data pipelines more robust and reliable.
+#' The *datatagr* package provides tools to help label and validate data. The
+#' 'datatagr' class adds variable level attributes to 'data.frame' columns. 
+#' Once tagged, these variables can be seamlessly used in downstream analyses, 
+#' making data pipelines more robust and reliable.
 #'
 #' @aliases datatagr
 #'
@@ -12,31 +12,31 @@
 #'   * [make_datatagr()]: to create `datatagr` objects from a `data.frame` or a
 #'   `tibble`
 #'
-#'   * [set_labels()]: to change or add tagged variables in a `datatagr`
+#'   * [set_labels()]: to change or add labelled variables in a `datatagr`
 #'
 #'   * [labels()]: to get the list of labels of a `datatagr`
 #'
 #'   * [labels_df()]: to get a `data.frame` of all tagged variables
 #'
-#'   * [lost_tags_action()]: to change the behaviour of actions where tagged
-#'   variables are lost (e.g. removing columns storing tagged variables) to
+#'   * [lost_tags_action()]: to change the behaviour of actions where labelled
+#'   variables are lost (e.g. removing columns storing labelled variables) to
 #'   issue warnings, errors, or do nothing
 #'
 #'   * [get_lost_tags_action()]: to check the current behaviour of actions where
-#'   tagged variables are lost
+#'   labelled variables are lost
 #'
 #' @section Dedicated methods:
 #'
 #'   Specific methods commonly used to handle `data.frame` are provided for
 #'   `datatagr` objects, typically to help flag or prevent actions which could
-#'   alter or lose tagged variables (and may thus break downstream data
+#'   alter or lose labelled variables (and may thus break downstream data
 #'   pipelines).
 #'
 #'   * `names() <-` (and related functions, such as [dplyr::rename()]) will
-#'   rename tags as needed
+#'   rename labels as needed
 #'
 #'   * `x[...] <-` and `x[[...]] <-` (see [sub_datatagr]): will adopt the
-#'    desired behaviour when tagged variables are lost
+#'    desired behaviour when labelled variables are lost
 #'
 #'   * `print()`: prints info about the `datatagr` in addition to the
 #'   `data.frame` or `tibble`
@@ -45,26 +45,26 @@
 #'
 #' # using base R style
 #' x <- make_datatagr(cars[1:50, ],
-#'   mph = "speed",
-#'   distance = "dist"
+#'   speed = "Miles per hour",
+#'   dist = "Distance in miles"
 #' )
 #' x
 #'
-#' ## check tagged variables
+#' ## check labelled variables
 #' labels(x)
 #'
 #' ## robust renaming
 #' names(x)[1] <- "identifier"
 #' x
 #'
-#' ## example of dropping tags by mistake - default: warning
+#' ## example of dropping labels by mistake - default: warning
 #' x[, 2]
 #'
-#' ## to silence warnings when taggs are dropped
+#' ## to silence warnings when labels are dropped
 #' lost_tags_action("none")
 #' x[, 2]
 #'
-#' ## to trigger errors when taggs are dropped
+#' ## to trigger errors when labels are dropped
 #' # lost_tags_action("error")
 #' # x[, 2:5]
 #'
@@ -73,30 +73,32 @@
 #'
 #' # using tidyverse style
 #'
-#' ## example of creating a datatagr, adding a new variable, and adding a tag
+#' ## example of creating a datatagr, adding a new variable, and adding a label
 #' ## for it
 #'
 #' if (require(dplyr) && require(magrittr)) {
 #'   x <- cars %>%
 #'     tibble() %>%
 #'     make_datatagr(
-#'       mph = "speed",
-#'       distance = "dist"
+#'       speed = "Miles per hour",
+#'       dist = "Distance in miles"
 #'     ) %>%
 #'     mutate(result = if_else(speed > 50, "fast", "slow")) %>%
-#'     set_labels(ticket = "result")
-#'
+#'     set_labels(result = "Ticket yes/no")
+#'     
 #'   head(x)
 #'
 #'   ## extract labelled variables
 #'   x %>%
-#'     select(has_label(c("mph", "distance")))
+#'     select(has_label(c("Ticket yes/no")))
 #'
+#'   ## Retrieve all labels
 #'   x %>%
 #'     labels()
 #'
+#'   ## Select based on variable name
 #'   x %>%
-#'     select(starts_with("dist"))
+#'     select(starts_with("speed"))
 #' }
 #'
 #' @keywords internal

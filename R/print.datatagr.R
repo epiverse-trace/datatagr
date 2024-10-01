6e7a5a5eba87e09e864ecb083c61890c579f5dd4
Chris Hartgerink <chris@libscie.org>
2024-10-01 15:36:34 +0200

    Consistent printing

--- R/print.datatagr.R
+++ R/print.datatagr.R
@@ -38,7 +38,7 @@ print.datatagr <- function(x, ...) {
   label_names <- names(label_values)
 
   # Construct the labels_txt string from the filtered pairs
-  labels_txt <- paste(label_names, label_values, sep = "-", collapse = ", ")
+  labels_txt <- paste(label_names, label_values, sep = " - ", collapse = ", ")
 
   if (labels_txt == "") {
     labels_txt <- "[no labelled variables]"
